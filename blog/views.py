ae3733704555c3249db44a29640430f8bb31801a
N'BE SORO <soro4827@gmail.com>
2023-01-08 23:16:52 +0000

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,19 @@ logger = logging.getLogger(__name__)
 @cache_page(300)
 @vary_on_cookie
 def index(request):
-    posts = Post.objects.all() #filter(published_at__lte=timezone.now())
+    #posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
+    """
+    posts = (
+        Post.objects.filter(published_at__lte=timezone.now())
+            .select_related("author")
+            .only("title", "summary", "content", "author", "published_at", "slug")
+    )
+    """
+    posts = (
+        Post.objects.filter(published_at__lte=timezone.now())
+        .select_related("author")
+        .defer("created_at", "modified_at")
+    )
     logger.debug("Got %d posts", len(posts))
 
     return render(request, "blog/index.html", {"posts": posts})
@@ -43,3 +55,7 @@ def post_detail(request, slug):
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
+
+def get_ip(request):
+    from django.http import HttpResponse
+    return HttpResponse(request.META['REMOTE_ADDR'])
