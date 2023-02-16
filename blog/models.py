9357ac0d4d6451375b25ba536a89b9e3886f12e8
N'BE SORO <soro4827@gmail.com>
2023-02-16 15:08:20 +0000

    Finish pagination and django filter

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,8 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.value
+    class Meta:
+        ordering = ["value"]
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
