2748e6e01c261dd4af5d277861734c90b0dda5bc
N'BE SORO <soro4827@gmail.com>
2023-02-28 11:34:42 +0000

    Finish ReactJS

--- static/blog/blog.js
+++ static/blog/blog.js
@@ -1,51 +1,37 @@
-
-class Greeter {
-  constructor (name) {
-    this.name = name
-  }
-
-  getGreeting () {
-    if (this.name === undefined) {
-      return 'Hello, no name'
+class ClickButton extends React.Component {
+    state = {
+      wasClicked: false
     }
 
-    return 'Hello, ' + this.name
-  }
-
-  showGreeting (greetingMessage) {
-    console.log(greetingMessage)
-  }
-
-  greet () {
-    this.showGreeting(this.getGreeting())
-  }
-}
-
-const g = new Greeter('Patchy')  // Put your name here if you like
-g.greet()
-
-function resolvedCallback(data) {
-  console.log('Resolved with data ' +  data)
-}
-
-function rejectedCallback(message) {
-  console.log('Rejected with message ' + message)
-}
-
-const lazyAdd = function (a, b) {
-  const doAdd = (resolve, reject) => {
-    if (typeof a !== "number" || typeof b !== "number") {
-      reject("a and b must both be numbers")
-    } else {
-      const sum = a + b
-      resolve(sum)
+    handleClick () {
+      this.setState(
+        {wasClicked: true}
+      )
     }
-  }
 
-  return new Promise(doAdd)
+    render () {
+      let buttonText
+
+      if (this.state.wasClicked)
+        buttonText = 'Clicked!'
+      else
+        buttonText = 'Click Me'
+
+      return React.createElement(
+        'button',
+        {
+          className: 'btn btn-primary mt-2',
+          onClick: () => {
+            this.handleClick()
+          }
+        },
+        buttonText
+      )
+    }
 }
 
-const p = lazyAdd(3, 4)
-p.then(resolvedCallback, rejectedCallback)
-
-lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
